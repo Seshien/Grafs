963434da64021ddca3a592395a6caeeae2f1e32a
Seshien <maciek22999@wp.pl>
2019-04-22 18:23:39 +0200

    Ja pierdole ile z tym bylo robotyv2

--- Grafs/Gen.cpp
+++ Grafs/Gen.cpp
@@ -28,11 +28,8 @@ void createR(std::vector<int> & nodes, std::vector<std::pair<int, int>> & relati
 		node1 = rand() % nodes.size();
 		node2 = rand() % nodes.size();
 		if (node1 == node2) continue;
-		if (!(relations.empty()))
-		{
-			if (exCheck(std::make_pair(node1, node2), relations)) continue;
-			if (exCheck(std::make_pair(node2, node1), relations)) continue;
-		}
+		//if (!(relations.empty()))
+			//if (exCheck(std::make_pair(node1, node2), relations)) continue;
 
 		relations.push_back(std::make_pair(node1, node2));
 		std::cout << "DFS Check" << std::endl;
@@ -46,11 +43,15 @@ void createR(std::vector<int> & nodes, std::vector<std::pair<int, int>> & relati
 
 }
 //Sprawdza czy istnieje juz taka relacja
-bool exCheck(std::pair<int, int > rel,const std::vector<std::pair<int, int>> & relations)
+bool exCheck(std::pair<int, int > rel, std::vector<std::pair<int, int>> & relations)
 {
-	for (std::vector<std::pair<int, int>>::const_iterator it = relations.begin(); it != relations.end(); it++)
-	if (rel==*it) 
-		return 1;
+	for (int i = 0; i < relations.size(); i++)
+	{
+		if (rel == relations[i])
+			return 1;
+		if ((rel.first == relations[i].second) && (rel.second == relations[i].first))
+			return 1;
+	}
 	return 0;
 }
 bool exCheck(int value, const std::vector<int> & searched)
@@ -67,7 +68,7 @@ bool cycleCheckF(const std::vector<std::pair<int, int>> & relations)
 	std::vector<std::pair<int, int>> first = cycleSearch(relations);
 	if (first.empty())
 	{
-		std::cout << "Nie mozna bylo znalezc wiercholka zerowego" << std::endl;
+		std::cout << "Nie mozna bylo znalezc wiercholka zerowego lub znaleziono krotka petle/multigraf." << std::endl;
 		return 1;
 	}
 	for (auto i : first)
@@ -134,21 +135,34 @@ std::vector<std::pair<int, int>> cycleFind(const std::vector<std::pair<int, int>
 std::vector<std::pair<int, int>> cycleSearch(const std::vector<std::pair<int, int>> & relations)
 {
 	std::vector<std::pair<int, int>> first;
+	std::vector<std::pair<int, int>> empty;
 	bool repeat = 0;
 	for (int i = 0; i < relations.size(); i++)
-		for (int j = 0; j <= relations.size(); j++)
-			if (j == relations.size())
-			{
-				repeat = 0;
-				for (int k = 0; k < first.size(); k++)
-					if (relations[k].first == relations[i].first)
-						repeat = 1;
-				if (repeat == 0)
-					first.push_back(std::make_pair(relations[i].first, i));
-			}
+	{
+		repeat = 0;
+		for (int j = 0; j < relations.size(); j++)
+		{
+			if (i == j)
+				continue;
+			if (relations[j] == relations[i])
+				return empty;
+			if ((relations[j].first == relations[i].second) && (relations[j].second == relations[i].first))
+				return empty;
+			if (relations[i].first == relations[j].second)
+				repeat = 1;
+		}
+		if (repeat == 0)
+		{
+			repeat = 0;
+			for (int k = 0; k < first.size(); k++)
+				if (relations[k].first == relations[i].first)
+					repeat = 1;
+			if (repeat == 0)
+				first.push_back(std::make_pair(relations[i].first, i));
+			continue;
+		}
+	}
 
-			else if (relations[i].first == relations[j].second)
-				break;
 	if (first.empty())
 		std::cout << "Something broken" << std::endl;
 	return first;
